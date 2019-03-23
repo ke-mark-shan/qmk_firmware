726bcf4311d24c9a58b5c518db791b566930a750
Mark <mark>
2019-03-23 15:57:14 -0400

    zeal60 mapping

---
+++ keyboards/zeal60/markshan/keymap.c
@@ -0,0 +1,35 @@
+// Default layout for Zeal60
+#include QMK_KEYBOARD_H
+
+
+#define _BASE_LAYER 0
+#define _FN_LAYER 1
+#define _SETUP_LAYER 15
+
+#define XXXXX KC_NO
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+// FN_MO13 FN_MO23
+
+[_BASE_LAYER] = LAYOUT_60_ansi(	
+	KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
+	KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+	MO(_FN_LAYER), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
+	KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+	KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT),
+
+[_FN_LAYER] = LAYOUT_60_ansi(
+	KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL ,
+	MO(_SETUP_LAYER), XXXXX, KC_UP,   XXXXX, XXXXX, XXXXX, XXXXX, KC_INS, KC_HOME, KC_PGUP, XXXXX , KC_VOLD, KC_VOLU, XXXXX,
+	XXXXX, KC_LEFT, KC_DOWN, KC_RGHT, XXXXX, XXXXX, XXXXX, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_MUTE, XXXXX,
+	XXXXX, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX, KC_DEL, KC_END, KC_PGDN, XXXXX, XXXXX,
+	XXXXX, XXXXX, XXXXX,                            XXXXX,                            XXXXX, XXXXX, XXXXX, XXXXX),
+
+[_SETUP_LAYER] = LAYOUT_60_ansi(
+	EF_OFF, EF_DEC,  EF_INC,  H1_DEC,  H1_INC,  H2_DEC,  H2_INC,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, BR_DEC,  BR_INC,  KC_TRNS,
+	KC_TRNS, KC_TRNS, KC_TRNS, S1_DEC,  S1_INC,  S2_DEC,  S2_INC,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, ES_DEC,  ES_INC,  KC_TRNS,
+	KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+	KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+	KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+
+};
