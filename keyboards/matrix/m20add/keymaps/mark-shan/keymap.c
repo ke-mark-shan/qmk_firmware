9e307780d344d1cabe5e4a95bc74be3446113903
Mark Shan <mark.shan@shopify.com>
2020-10-20 13:43:46 -0400

    first matrix 2.0add

---
+++ keyboards/matrix/m20add/keymaps/mark-shan/keymap.c
@@ -0,0 +1,28 @@
+/**
+ * keymap.c
+ */
+
+#include QMK_KEYBOARD_H
+
+#define XXXXX KC_NO
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+	[0]=LAYOUT_tkl_ansi_tsangan(
+
+		KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,   KC_PSCR, KC_SLCK, LT(1,KC_PAUS),
+
+		KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL,  KC_BSPC,   KC_INS, KC_HOME, KC_PGUP,
+		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,   KC_DEL, KC_END, KC_PGDN,
+		MO(1),   KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,     KC_ENT,
+		KC_LSFT,     KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,      KC_RSFT,            KC_UP,
+		KC_LCTL, KC_LGUI, KC_LALT,                 KC_SPC,                    KC_LGUI, XXXXX, KC_RCTL,  KC_LEFT, KC_DOWN, KC_RGHT),
+
+	[1]=LAYOUT_tkl_ansi_tsangan(
+		_______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______,
+
+		_______, KC_F13, KC_F14, KC_F15, KC_F18, KC_F16, KC_F19, KC_F17, KC_F20, KC_F21, KC_F22, _______, _______, _______,          _______, _______, _______,
+		_______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, KC_VOLD, KC_VOLU, RESET,          _______, _______, _______,
+		_______,   _______, _______, KC_F24, _______, _______, _______, _______, _______, _______, _______, KC_MUTE,          _______,
+		_______,         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,           _______,                   KC_VOLU,
+		_______, _______, _______,                           _______,                                       _______, _______, _______,          KC_MPRV, KC_VOLD, KC_MNXT),
+};
